804f07a0a63dc48e1ec48bd54bb29edd29443e4e
raka1boy <artemka2015kek@gmail.com>
2025-03-18 03:46:28 +0300

    Init commit

---
+++ src/main.zig
@@ -0,0 +1,27 @@
+pub fn main() !void {
+    std.debug.print("All your {any} are belong to us.\n", .{});
+    // const stdout_file = std.io.getStdOut().writer();
+    // var bw = std.io.bufferedWriter(stdout_file);
+    // const stdout = bw.writer();
+    // try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    // try bw.flush();
+}
+test "simple test" {
+    var list = std.ArrayList(i32).init(std.testing.allocator);
+    defer list.deinit(); // Try commenting this out and see if zig detects the memory leak!
+    try list.append(42);
+    try std.testing.expectEqual(@as(i32, 42), list.pop());
+}
+
+test "fuzz example" {
+    const Context = struct {
+        fn testOne(context: @This(), input: []const u8) anyerror!void {
+            _ = context;
+            // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
+            try std.testing.expect(!std.mem.eql(u8, "canyoufindme", input));
+        }
+    };
+    try std.testing.fuzz(Context{}, Context.testOne, .{});
+}
+
+const std = @import("std");
